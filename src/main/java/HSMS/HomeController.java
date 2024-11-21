4cdb140b0ea876092c17f978fe17f5cb98fc0192
Vedant Mane <118092069+vedxnt10@users.noreply.github.com>
2024-11-22 00:26:26 +0530

    Update HomeController.java

--- src/main/java/HSMS/HomeController.java
+++ src/main/java/HSMS/HomeController.java
@@ -17,10 +17,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-/**
- *
- * @author Yash
- */
 @Controller()
 public class HomeController {
 
